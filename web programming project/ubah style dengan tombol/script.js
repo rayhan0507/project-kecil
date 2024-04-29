fa4f3ee4885ed4e6a784ed6cd0bf76006456d929
ryxa <141266480+rayhan0507@users.noreply.github.com>
2024-04-30 01:30:53 +0700

    uji coba merubah style dengan sebuah tombol

---
+++ web programming project/ubah style dengan tombol/script.js
@@ -0,0 +1,14 @@
+function background() {
+    const el = document.querySelector(".background.color");
+    el.classList.add("background-grey");  //background() background-grey        
+}
+
+function textcolor() {
+    const text = document.querySelector(".text.color");
+    text.classList.add("color-aqua");
+}
+
+function border() {
+    const border = document.querySelector(".border.radius") //html
+    border.classList.add("border-radius") // css
+}
