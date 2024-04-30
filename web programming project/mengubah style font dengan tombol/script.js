b3108aaae0c9097023d366e81739d4d539cd6da4
ryxa <141266480+rayhan0507@users.noreply.github.com>
2024-04-30 21:41:48 +0700

    mengubah font, style, weight dengan tombol

---
+++ web programming project/mengubah style font dengan tombol/script.js
@@ -0,0 +1,15 @@
+function italic() {
+    const text = document.querySelector("#font");
+    text.classList.toggle("italic");
+    
+}
+
+function bold() {
+    const bold = document.querySelector("#font");
+    bold.classList.toggle("bold");
+}   
+
+function fontsaya() {
+    const fontsaya = document.querySelector("#font")
+    fontsaya.classList.toggle("fontsaya")
+}
