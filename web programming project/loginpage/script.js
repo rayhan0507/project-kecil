cda1780de510a51b8f69cf0549feda0f9786aea9
ryxa <141266480+rayhan0507@users.noreply.github.com>
2024-05-06 00:31:15 +0700

    mencoba membuat login page dengan flexbox

---
+++ web programming project/loginpage/script.js
@@ -0,0 +1,6 @@
+function submit() {
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    console.log(`username: ${username}`);
+    console.log(`password: ${password}`);
+}
