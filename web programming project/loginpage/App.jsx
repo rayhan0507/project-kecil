1a38facb5777c93dc143e5b8b7d986d237654d84
ryxa <141266480+rayhan0507@users.noreply.github.com>
2024-07-01 00:33:13 +0700

    menambah fitur jika username di bawah 8 digit muncul pengingat

--- web programming project/loginpage/App.jsx
+++ web programming project/loginpage/App.jsx
@@ -9,68 +9,76 @@ class App extends React.Component{
         this.state = {
             username: "",
             password: "",
-            displayUsername: ""
-
+            displayUsername: "",
+            validationMessage: ""
         };
     }
-        handleregister = () => {
+
+    handleregister = () => {
+        const { username } = this.state;
+        if (username.length < 8) {
             this.setState({
-                displayUsername: this.state.username,
-            })
-        };
-
-        handleusername = (event) => {
+                validationMessage: "Username anda harus minimal 8 digit"
+            });
+        } else {
             this.setState({
-                username: event.target.value,
-            })
-        };
-
-        handlepassword = (event) => {
-            this.setState({
-                password: event.target.value,
-            })
-        };
-
-        //render
-        render() {
-            const { username, password, displayUsername } = this.state;
-            return(
-                <div className="container">
-                    <h1 className='font'>Register</h1>
-                    <input
-                    type="text"
-                    className='font input'
-                    placeholder='Username'
-                    value={username}
-                    onChange={this.handleusername}
-                    /><LuUserCircle className='icon white'/><br />
-
-                    <input
-                    type="password"
-                    className='font input'
-                    placeholder='Password'
-                    value={password}
-                    onChange={this.handlepassword}
-                    /><IoLockClosedOutline className='icon white' />
-                    <br/>
-
-
-                    <div className='link'>
-                        <label className='font white checkbox1'><input type="checkbox" className='cursor checkbox'/>Remember me</label>
-                        <a className='blue font cursor forgot' href="#">forgot your password?</a>
-                    </div>
-
-                    <button type="submit" id="kosong" className='font input cursor bold' onClick={this.handleregister}>LOGIN</button><br />
-                   
-                    <p className='font white'>{displayUsername && `username anda adalah ${this.state.username}`}</p>
-                  
-                    <div>
-                       <p className='font white'>have an account?<a className="font cursor blue" href="#">Login</a></p>
-                    </div>
-                </div>
-            )
+                displayUsername: username,
+                validationMessage: ""
+            });
         }
-    
+    };
+
+    handleusername = (event) => {
+        this.setState({
+            username: event.target.value,
+        });
+    };
+
+    handlepassword = (event) => {
+        this.setState({
+            password: event.target.value,
+        });
+    };
+
+    //render
+    render() {
+        const { username, password, displayUsername, validationMessage } = this.state;
+        return(
+            <div className="container">
+                <h1 className='font'>Register</h1>
+                <input
+                type="text"
+                className='font input'
+                placeholder='Username'
+                value={username}
+                onChange={this.handleusername}
+                /><LuUserCircle className='icon white'/><br />
+
+                <input
+                type="password"
+                className='font input'
+                placeholder='Password'
+                value={password}
+                onChange={this.handlepassword}
+                /><IoLockClosedOutline className='icon white' />
+                <br/>
+
+                <div className='link'>
+                    <label className='font white checkbox1'><input type="checkbox" className='cursor checkbox'/>Remember me</label>
+                    <a className='blue font cursor forgot' href="#">forgot your password?</a>
+                </div>
+
+                <button type="submit" id="kosong" className='font cursor bold' onClick={this.handleregister}>LOGIN</button><br />
+
+                <p className='font white'>{validationMessage}</p>
+                <p className='font white'>{displayUsername && `username anda adalah ${this.state.username}`}</p>
+                
+                <div>
+                   <p className='font white'>have an account?<a className="font cursor blue" href="#">Login</a></p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
